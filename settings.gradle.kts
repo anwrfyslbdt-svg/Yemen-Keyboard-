fb1b38bb7dd41f4ab3745c8206b6330fc487a810
anwrfyslbdt-svg <anwrfyslbdt@gmail.com>
2026-08-25 19:45:32 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,7 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -19,6 +20,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Android Open Soruce Keyboard"
+rootProject.name = "Yemen Keyboard"
+
 include(":library")
-include(":simplekeyboard")
