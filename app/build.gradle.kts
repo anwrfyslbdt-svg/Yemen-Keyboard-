efd99b839fe4f93593656044d2afce397c4b805c
anwrfyslbdt-svg <anwrfyslbdt@gmail.com>
2026-08-25 20:46:49 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.yemen.keyboard"
-        minSdk = 23
+        minSdk = 24
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
