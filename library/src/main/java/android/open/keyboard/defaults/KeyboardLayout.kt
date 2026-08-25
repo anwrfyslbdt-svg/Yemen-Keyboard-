ec0a1aa643fb86c93efda99f5d06e41d46e4c3bc
anwrfyslbdt-svg <anwrfyslbdt@gmail.com>
2026-08-25 23:38:20 +0300

    Update KeyboardLayout.kt

--- library/src/main/java/android/open/keyboard/defaults/KeyboardLayout.kt
+++ library/src/main/java/android/open/keyboard/defaults/KeyboardLayout.kt
@@ -36,7 +36,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-
+import androidx.compose.ui.layout.ContentScale
 @Extension(ID = "android.open.keyboard.defaults.KeyboardLayout", description = "Simple Compose Keyboard Layout")
 class KeyboardLayout : AbstractComposeLayout() {
     /**
