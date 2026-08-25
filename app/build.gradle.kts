687c036f0ddb11d2d45415fcb38eb0c7d7990d62
anwrfyslbdt-svg <anwrfyslbdt@gmail.com>
2026-08-25 20:28:15 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -15,20 +14,9 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
-
-    buildFeatures {
-        compose = true
-    }
 }
 
 dependencies {
     implementation(project(":library"))
-
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
 }
